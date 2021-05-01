0b769d8041d8de032c531c9ea17ebb9d8de23500
adam <adam.ondrejkovic@gmail.com>
2021-05-01 18:15:07 +0200

    Mock user view with functions to read through the excel and csv files + browse on the internet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("view/LoginPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         primaryStage.sceneProperty().addListener((observableValue, oldScene, newScene) -> {
