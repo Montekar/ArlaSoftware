6f8313cc09064bcccc1d210c0a8cab1493103998
adam <adam.ondrejkovic@gmail.com>
2021-05-04 11:04:10 +0200

    Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/UserMockView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml"));
         primaryStage.initStyle(StageStyle.UNDECORATED);
 
         primaryStage.sceneProperty().addListener((observableValue, oldScene, newScene) -> {
